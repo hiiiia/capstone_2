230b76f4ac118b0e2a2e9eacda475c551f48098c
hiiiia <90683622+hiiiia@users.noreply.github.com>
2024-03-29 12:42:57 +0900

    Update main_test.py

--- gym-examples-main/gym_examples/main_test.py
+++ gym-examples-main/gym_examples/main_test.py
@@ -6,10 +6,11 @@ env = gym.make('gym_examples/GridWorld-v0',render_mode='human' ,size=10)
 
 observation, info = env.reset()
 
-for _ in range(1000):
+for i in range(1000):
     action = env.action_space.sample()  # agent policy that uses the observation and info
     observation, reward, terminated, truncated, info = env.step(action)
 
     if terminated or truncated:
         observation, info = env.reset()
+    print(f'Epoch {i} : Done / Reward : {reward}')
 env.close()
