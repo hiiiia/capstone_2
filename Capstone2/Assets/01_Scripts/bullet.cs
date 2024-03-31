39f90139fa7da4d8a68fa5b41146343a600dc388
oddturtle2546 <104830860+JHuiJung@users.noreply.github.com>
2024-04-01 00:57:29 +0900

    프로젝트 설치 및 플레이어, 공격 구현

---
+++ Capstone2/Assets/01_Scripts/bullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class bullet : MonoBehaviour
+{
+
+    public float power = 8f;
+    public void Shoot(bool isLeft)
+    {
+        Vector3 direction = isLeft ? Vector3.left : Vector3.right;
+
+        transform.DOMove(transform.position + direction * 20f, 1.25f).SetEase(Ease.Linear).OnComplete(() =>
+        {
+            Destroy(gameObject);
+        });
+    }
+
+}
