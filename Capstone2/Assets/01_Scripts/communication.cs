4c0d186902b9f84d881396059492a315661654d7
hiiiia <90683622+hiiiia@users.noreply.github.com>
2024-04-02 19:57:05 +0900

    d
    d

---
+++ Capstone2/Assets/01_Scripts/communication.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+
+public class communication : MonoBehaviour
+{
+    Player player;
+    private float[] sending_bin_code = new float[7];
+
+
+    private TcpClient clientSocket;
+    private Thread receiveThread;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = Player.inst;
+
+        string serverHost = "127.0.0.1";
+        int serverPort = 12345;
+
+        // 소켓 생성
+        clientSocket = new TcpClient();
+
+        // 서버에 연결
+        clientSocket.Connect(serverHost, serverPort);
+        Debug.Log("서버에 연결됨");
+
+        // 데이터를 받는 스레드 시작
+        receiveThread = new Thread(new ThreadStart(ReceiveData));
+        receiveThread.Start();
+
+
+    }
+
+    void OnDestroy()
+    {
+        // 소켓과 스레드 종료
+        clientSocket.Close();
+        receiveThread.Abort();
+    }
+
+    void ReceiveData()
+    {
+        while (true)
+        {
+            // 서버로부터 데이터 수신
+            byte[] bytesReceived = new byte[1024];
+            int byteCount = clientSocket.GetStream().Read(bytesReceived, 0, bytesReceived.Length);
+            string dataReceived = Encoding.ASCII.GetString(bytesReceived, 0, byteCount);
+            Debug.Log("수신한 데이터: " + dataReceived);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        // up down left right 
+
+        for(int i =0; i < 7; i++)
+        {
+            sending_bin_code[i] = 0;
+        }
+
+
+
+        if (player.vertical > 0)
+            sending_bin_code[0] = 1;
+        else if (player.vertical < 0)
+            sending_bin_code[1] = 1;
+
+        if (player.horizontal > 0)
+            sending_bin_code[3] = 1;
+        else if(player.horizontal < 0)
+            sending_bin_code[2] = 1;
+
+        if (player.fire > 0)
+            sending_bin_code[4] = 1;
+
+        sending_bin_code[5] = player.transform.position.x;
+        sending_bin_code[6] = player.transform.position.y;
+        //sending_bin_code[7] = player.transform.position.z;
+
+        string codeString = "";
+
+        foreach (float code in sending_bin_code)
+        {
+            codeString += code.ToString() + ", ";
+        }
+
+        Debug.Log(codeString);
+
+        byte[] bytesToSend = Encoding.ASCII.GetBytes(codeString);
+        clientSocket.GetStream().Write(bytesToSend, 0, bytesToSend.Length);
+        Debug.Log("데이터 전송 완료");
+
+    }
+}
---
+++ gym-examples-main/gym_examples/communication.py
@@ -0,0 +1,47 @@
+import socket
+import threading
+
+def handle_client(client_socket):
+    while True:
+        # 클라이언트로부터 데이터 수신
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        print("수신한 데이터:", data.decode())
+
+        # 클라이언트에게 데이터 전송
+        #message = input("전송할 메시지를 입력하세요: ")
+        message = "Done"
+        
+        client_socket.send(message.encode())
+
+    # 클라이언트 소켓 종료
+    client_socket.close()
+
+def start_server():
+    # 호스트 및 포트 지정
+    HOST = '127.0.0.1'
+    PORT = 12345
+
+    # 소켓 생성
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # 포트에 바인딩
+    server_socket.bind((HOST, PORT))
+
+    # 클라이언트의 연결 요청 대기
+    server_socket.listen()
+
+    print("서버가 시작되었습니다.")
+
+    while True:
+        # 클라이언트 연결 수락
+        client_socket, client_address = server_socket.accept()
+        print(f"{client_address}에서 연결됨")
+
+        # 클라이언트 핸들링을 위한 스레드 시작
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
+
+# 서버 시작
+start_server()
