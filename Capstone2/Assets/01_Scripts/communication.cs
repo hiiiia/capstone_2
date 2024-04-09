2f5e12a910d7ad848534c9f3b1b186f182d7d3bd
hiiiia <90683622+hiiiia@users.noreply.github.com>
2024-04-09 10:34:19 +0900

    Update communication.cs

--- Capstone2/Assets/01_Scripts/communication.cs
+++ Capstone2/Assets/01_Scripts/communication.cs
@@ -15,6 +15,7 @@ public class communication : MonoBehaviour
     private TcpClient clientSocket;
     private Thread receiveThread;
 
+    private float frame_timer = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +36,7 @@ public class communication : MonoBehaviour
         receiveThread = new Thread(new ThreadStart(ReceiveData));
         receiveThread.Start();
 
+        frame_timer = 0.0f;
 
     }
 
@@ -62,45 +64,56 @@ public class communication : MonoBehaviour
     void Update()
     {
 
+        if(frame_timer <= 0.01)
+        {
+
+            for (int i = 0; i < 7; i++)
+            {
+                sending_bin_code[i] = 0;
+            }
+
+
+
+            if (player.vertical > 0)
+                sending_bin_code[0] = 1;
+            else if (player.vertical < 0)
+                sending_bin_code[1] = 1;
+
+            if (player.horizontal > 0)
+                sending_bin_code[3] = 1;
+            else if (player.horizontal < 0)
+                sending_bin_code[2] = 1;
+
+            if (player.fire > 0)
+                sending_bin_code[4] = 1;
+
+            sending_bin_code[5] = player.transform.position.x;
+            sending_bin_code[6] = player.transform.position.y;
+            //sending_bin_code[7] = player.transform.position.z;
+
+            string codeString = "";
+
+            foreach (float code in sending_bin_code)
+            {
+                codeString += code.ToString() + ", ";
+            }
+
+            Debug.Log(codeString);
+
+            byte[] bytesToSend = Encoding.ASCII.GetBytes(codeString);
+            clientSocket.GetStream().Write(bytesToSend, 0, bytesToSend.Length);
+            Debug.Log("데이터 전송 완료");
+
+            frame_timer += 0.001f;
+
+        }
+        else
+        {
+            frame_timer = 0.0f;
+        }
+    }
+
 
         // up down left right 
 
-        for(int i =0; i < 7; i++)
-        {
-            sending_bin_code[i] = 0;
-        }
-
-
-
-        if (player.vertical > 0)
-            sending_bin_code[0] = 1;
-        else if (player.vertical < 0)
-            sending_bin_code[1] = 1;
-
-        if (player.horizontal > 0)
-            sending_bin_code[3] = 1;
-        else if(player.horizontal < 0)
-            sending_bin_code[2] = 1;
-
-        if (player.fire > 0)
-            sending_bin_code[4] = 1;
-
-        sending_bin_code[5] = player.transform.position.x;
-        sending_bin_code[6] = player.transform.position.y;
-        //sending_bin_code[7] = player.transform.position.z;
-
-        string codeString = "";
-
-        foreach (float code in sending_bin_code)
-        {
-            codeString += code.ToString() + ", ";
-        }
-
-        Debug.Log(codeString);
-
-        byte[] bytesToSend = Encoding.ASCII.GetBytes(codeString);
-        clientSocket.GetStream().Write(bytesToSend, 0, bytesToSend.Length);
-        Debug.Log("데이터 전송 완료");
-
-    }
 }
